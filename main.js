c612a6930e4756fb46d92b013564ac3bae66c18a
davidovicd <davidovicd620@gmail.com>
2023-07-27 10:08:34 +0200

    Added functionallity to change board size

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
-document.addEventListener("DOMContentLoaded", createBoard(16))
+document.addEventListener("DOMContentLoaded", function(){
+  let select = document.querySelector(".select")
+  select.addEventListener("click", function() {
+    let size = getSize()
+    createBoard(size)
+  })
+})
 
 function createBoard (size)  {
   let container = document.querySelector(".container");
-  container.style.gridTemplateColumns = `repeat (${size}, 1fr)`;
+  container.style["grid-template-columns"] = `repeat(${size}, 1fr)`;
+  container.style["grid-template-rows"] = `repeat(${size}, 1fr)`;
   let numDiv = size * size;
 
   for (let i = 0; i < numDiv ; i++){
@@ -17,3 +24,18 @@ function createBoard (size)  {
 }
 
 
+function getSize(){
+  let input = prompt("Insert board size: ")
+  let message = document.querySelector("#message")
+  if (input == ""){
+    message.textContent = "Please provide a number"
+  }
+  else if (input < 1 || input > 100){
+    message.textContent = "Please provide a number between 10 and 100"
+  }
+  else{
+    message.textContent = "Board created"
+  }
+  return input
+
+}
