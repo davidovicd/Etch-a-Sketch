f0f52090c70ced01d33a2f7eac7440a3cf4e168f
davidovicd <davidovicd620@gmail.com>
2023-07-27 13:15:06 +0200

    Added functionallity to the buttons inside footer

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+let color = "#000"
+let container = document.querySelector(".container");
 
 document.addEventListener("DOMContentLoaded", function(){
+  createBoard(16)
   let select = document.querySelector(".select")
   select.addEventListener("click", function() {
     let size = getSize()
     createBoard(size)
   })
+  let reset = document.querySelector(".reset")
+  reset.addEventListener("click", function(){
+    let child = container.lastElementChild;
+    while (child) {
+      container.removeChild(child);
+      child = container.lastElementChild;
+    }
+    createBoard(16)
+  })
 })
 
 function createBoard (size)  {
-  let container = document.querySelector(".container");
+  
   container.style["grid-template-columns"] = `repeat(${size}, 1fr)`;
   container.style["grid-template-rows"] = `repeat(${size}, 1fr)`;
   let numDiv = size * size;
@@ -17,9 +29,8 @@ function createBoard (size)  {
     let div = document.createElement("div");
     container.appendChild(div);
 
-    div.addEventListener("mouseover", ()=> {
-      div.style.backgroundColor = "black"
-    })
+    div.addEventListener("mouseover", colorDiv)
+
   }
 }
 
@@ -35,7 +46,22 @@ function getSize(){
   }
   else{
     message.textContent = "Board created"
+    return input
   }
-  return input
+}
 
+function colorDiv(){
+  if (color == "random"){
+    let r = Math.floor(Math.random() * 256); 
+    let g = Math.floor(Math.random() * 256); 
+    let b = Math.floor(Math.random() * 256);
+    this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  }
+  else{
+    this.style.backgroundColor = "#000"
+  }
+}
+
+function setColor(colorChoice){
+  color = colorChoice
 }
