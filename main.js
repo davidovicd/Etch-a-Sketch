2209bd9c869c5710271dee3089af1548c5ea70f0
davidovicd <davidovicd620@gmail.com>
2023-07-27 09:31:21 +0200

    Created a 16x16 div board

---
+++ main.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", createBoard(16))
+
+function createBoard (size)  {
+  let container = document.querySelector(".container");
+  container.style.gridTemplateColumns = `repeat (${size}, 1fr)`;
+  let numDiv = size * size;
+
+  for (let i = 0; i < numDiv ; i++){
+    let div = document.createElement("div");
+    container.appendChild(div);
+  }
+}
+
+
